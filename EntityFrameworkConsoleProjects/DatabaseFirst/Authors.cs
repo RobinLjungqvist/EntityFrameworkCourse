2ec35f0c5a645f9d89c0936d20a3ebd08c5acfd2
Roppin.L <robin.ljungqvist@gmail.com>
2016-10-20 12:50:05 +0200

    Lab 2

--- EntityFrameworkConsoleProjects/DatabaseFirst/Authors.cs
+++ EntityFrameworkConsoleProjects/DatabaseFirst/Authors.cs
@@ -24,7 +24,7 @@ namespace DatabaseFirst
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string HomeTel { get; set; }
-        public Nullable<PaymentMethods> PaymetMethod { get; set; }
+        public Nullable<DatabaseFirst.PaymentMethods> PaymetMethod { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Titles> Titles { get; set; }
--- EntityFrameworkConsoleProjects/DatabaseFirst/Program.cs
+++ EntityFrameworkConsoleProjects/DatabaseFirst/Program.cs
@@ -10,6 +10,39 @@ namespace DatabaseFirst
     {
         static void Main(string[] args)
         {
+            var author = new Authors();
+
+            author.FirstName = "Tom";
+            author.LastName = "Clancy";
+            author.PaymetMethod = PaymentMethods.Bank;
+            author.HomeTel = "037112345";
+
+            var title = new Titles();
+
+            title.Title = "Splinter Cell";
+            title.Copyright = "Bonnier";
+            title.EditionNumber = 154;
+            title.ISBN = "123145747742414";
+
+            ////author.Titles.Add(title);
+
+
+
+            using (var ctx = new BooksContext())
+            {
+                ctx.Authors.Add(author);
+                ctx.SaveChanges();
+            }
+
+            using (var ctx = new BooksContext())
+            {
+                var authorToDisplay = ctx.Authors.Where(x => x.FirstName == "Tom").FirstOrDefault();
+
+                Console.WriteLine(authorToDisplay.FirstName + " " + authorToDisplay.LastName +
+                    ". Payment method: " + authorToDisplay.PaymetMethod);
+
+            }
+            Console.ReadKey();
         }
     }
 }
